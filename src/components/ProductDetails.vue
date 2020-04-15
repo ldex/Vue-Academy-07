27fc98fea54b61ea1bda6172e76802dce424e449
ldex <laurent.duveau+git@gmail.com>
2020-04-15 15:21:19 -0400

    after delete feature

---
+++ src/components/ProductDetails.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <section v-if="error">
+      {{error.message}}
+    </section>
+    <section v-else>
+      <div v-if="loading">
+          <h2>Loading...</h2>
+      </div>
+      <div v-else>
+        <h2>{{product.name | uppercase}}</h2>
+        <img :src="product.imageUrl ? product.imageUrl : 'https://placeimg.com/200/200/tech'" width="200" style="float:right" />
+        <h3>{{product.description}}</h3>
+        <p>Price: {{product.price | currency('$', 2, { symbolOnLeft: false })}}</p>
+        <p>Fixed price? {{product.fixedPrice}}</p>
+        <p>Discontinued? {{product.discontinued}}</p>
+        <p>Modified date: {{product.modifiedDate | formatDate('MM/DD/YYYY hh:mm') }}</p>  
+        <button @click="onDelete(product.id)">Delete</button>             
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import { mapState, mapActions } from 'vuex'
+
+    export default {
+        data () {
+            return {
+                error: null,
+                loading: false,
+                componentName: 'Product Details',
+            }
+        },
+        computed: {
+            ...mapState(['product']), // map `this.product` to `this.$store.state.product`
+        },
+        props: {
+            id: {
+                type:Number,
+                required:true
+            }
+        },
+        methods: {
+            onDelete(id) {
+                if(window.confirm('Are you sure ?')) {        
+                    this.deleteProduct(id)
+                    .then(() => {        
+                        console.log('The product has been deleted.');
+                        this.$router.push({ name: 'products'});
+                    })
+                    .catch(error => {
+                        console.log('There was an error:', error.response)
+                    });
+                }
+            },
+            ...mapActions(['fetchProduct','deleteProduct']) // map `this.fetchProduct(this.id)` to `this.$store.dispatch('fetchProduct', this.id)`
+        },
+        created() {
+          this.fetchProduct(this.id);
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,34 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import './assets/animate.css'
+import Vuelidate from "vuelidate"
+import Vue2Filters from 'vue2-filters'
+import formatDateFilter from './filters/formatDateFilter';
+import logOnCreatedMixin from './mixins/logOnCreatedMixin';
+import store from './store'
+
+Vue.mixin(logOnCreatedMixin);
+Vue.filter('formatDate', formatDateFilter);
+Vue.use(Vue2Filters);
+Vue.use(Vuelidate);
 
 Vue.config.productionTip = false
 
+// JavaScript runtime error 
+window.onerror = function(msg, src, linenum, colnum, error) {
+  console.log('[JavaScript Error Handler]: ' + msg + '- ' + error);
+  router.push({ name: 'error'});
+}
+
+// Vue related errors
+Vue.config.errorHandler = function (err, vm, info) {
+  console.log('[Vue Error Handler]: Error in ' + info + ': ' + err);
+  router.push({ name: 'error'});
+}
+
 new Vue({
-  render: h => h(App),
+  router,
+  store,
+  render: h => h(App)
 }).$mount('#app')
